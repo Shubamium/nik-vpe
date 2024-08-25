b548f18cdd49f6b1025330d7556621ac8513f120
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-08-26 05:49:35 +0700

    Fix ref

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header";
 import GeneralInfo from "./section/generalInfo/GeneralInfo";
-import GeneralPoints from "./section/generalPoints/generalPoints";
+import GeneralPoints from "./section/generalPoints/GeneralPoints";
 import HeroSection from "./section/heroSection/HeroSection";
 
 export default function Home() {
